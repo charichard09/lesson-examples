26de4a3b0ba88590df1a1b2485e80377f2c5770f
Richard <charichard09@gmail.com>
2022-10-19 10:52:19 -0700

    mad libs working

--- mad-libs/js/scripts.js
+++ mad-libs/js/scripts.js
@@ -1,19 +1,25 @@
 window.onload = function() {
-  // let divStoryObj = document.querySelector("div#story");
   let formObj = document.querySelector("form");
   
-  formObj.onsubmit = function(e) {
-    document.querySelector("span#person1a").innerText = "person 1";
-    document.querySelector("span#person1b").innerText = "person 1";
-    document.querySelector("span#person1c").innerText = "person 1";
-    document.querySelector("span#person2a").innerText = "person 2";
-    document.querySelector("span#person2b").innerText = "person 2";
-    document.querySelector("span#animal").innerText = "an animal";
-    document.querySelector("span#verb").innerText = "verb";
-    document.querySelector("span#noun").innerText = "noun";
-    document.querySelector("span#exclamation").innerText = "exclamation";
+  formObj.onsubmit = function(event) {
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").value;
+    const nounInput = document.getElementById("nounInput").value;
+  
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclamation").innerText = exclamationInput;
   
     document.querySelector("div#story").removeAttribute("class");
-    e.preventDefault();
+    event.preventDefault();
   };
 };
